f7c1a0c2740b07e70651bab367f659230f9c1cae
Daeyeon, Jo <repositivator@gmail.com>
2016-08-27 21:32:42 +0900

    후보 정보 출력 구현

--- elections/views.py
+++ elections/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Candidate
 
 def index(request):
-    return HttpResponse("HelloWorld")
+    candidates = Candidate.objects.all()
+
+    str = ''
+    for candidate in candidates:
+        str += "<p>{}기호 {}번({})<br>".format(candidate.name, candidate.party_number, candidate.area)
+        str += candidate.introduction+"</p>"
+
+    return HttpResponse(str)
 
