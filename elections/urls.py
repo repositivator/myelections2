2c4bd3061c70b719dec966cea91d7cb6c53ca316
Daeyeon, Jo <repositivator@gmail.com>
2016-08-28 15:27:01 +0900

    템플릿 상속 및 네비게이션 바 구현

--- elections/urls.py
+++ elections/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index),
+    url(r'^$', views.index, name='home'),
     url(r'^areas/(?P<area>[가-힣]+)/$', views.area),
     url(r'^areas/(?P<area>[가-힣]+)/results$', views.results),
     url(r'^polls/(?P<poll_id>\d+)/$', views.polls),
