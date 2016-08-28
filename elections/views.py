674bb93ead654ed534be61ce9533cc59fbce9d56
Daeyeon, Jo <repositivator@gmail.com>
2016-08-28 15:18:39 +0900

    후보별 지지율 출력 view 함수 구현

--- elections/views.py
+++ elections/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
+from django.db.models import Sum
 from .models import Candidate, Poll, Choice
 import datetime
 
@@ -38,10 +39,12 @@ def polls(request, poll_id):
 
     try:
         choice = Choice.objects.get(poll_id=poll_id, candidate_id=selection)
+        print("checked update")
         choice.votes += 1
-        chocie.save()
+        choice.save()
     except Exception:
         choice = Choice(poll_id = poll_id, candidate_id=selection, votes = 1)
+        print("checked new one")
         choice.save()
 
     return HttpResponseRedirect("/areas/{}/results".format(poll.area))
@@ -57,6 +60,18 @@ def results(request, area):
         result = {}
         result['start_date'] = poll.start_date
         result['end_date'] = poll.end_date
+
+        total_votes = Choice.objects.filter(poll_id=poll.id).aggregate(Sum('votes'))
+        result['total_votes'] = total_votes['votes__sum']
+
+        rates = []
+        for candidate in candidates:
+            try:
+                choice = Choice.objects.get(poll_id = poll.id, candidate_id=candidate.id)
+                rates.append(round(choice.votes*100/result['total_votes'],1))
+            except Exception:
+                rates.append(0)
+        result['rates'] = rates
 
         poll_results.append(result)
 
